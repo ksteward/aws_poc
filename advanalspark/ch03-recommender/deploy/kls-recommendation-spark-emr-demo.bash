3c1f5b5c8ef446fc3797a2b62b18c6cb46763c21
Steward <stewardk@685b3589ea1f.ant.amazon.com>
2015-09-15 15:57:23 -0400

    * now analyses data that has been made publicly available via the   s3://audioscrobbler-pds/ bucket * now gives more status messaging at the commandline when the deployment   script is executed. * this version has the Maven build step to rebuild the .jar file when   any Scala code has changed.

--- advanalspark/ch03-recommender/deploy/kls-recommendation-spark-emr-demo.bash
+++ advanalspark/ch03-recommender/deploy/kls-recommendation-spark-emr-demo.bash
@@ -1,11 +1,14 @@
 #!/bin/bash
+# Music Recommendation System EMR/Spark Demo
+# ===========================================
+# k. steward, 2015-09-15.
 # Automatically launch a Spark cluster on AWS, submit recommendation ML,
 # make some recommendations, then terminate cluster.
-# Assumptions;  AWS_SECRET_ACCESS_KEY & AWS_ACCESS_KEY_ID env vars are set.
-#
-# TODO:  speed up 10-fold by:
-#  a) increasing NUM_NODES to 15
-#  b) increasing instance-type from m1.large to c3.2xlarge (15 Gb RAM, 8 cores, SSD)
+# The Audioscrobbler publicly-available data used as input in the analysis
+# (and referenced within th Scala code) lives at s3://audioscrobbler-pds/
+# Assumptions;  AWS_SECRET_ACCESS_KEY & AWS_ACCESS_KEY_ID env vars are set
+# or configured into your AWS CLI installation.
+
 AMIVERSION=3.9.0
 INST_TYPE=c3.4xlarge
 INSTANCECORES=16
@@ -15,7 +18,7 @@ TOTALCORES=`echo "($NUMCORENODES + $NUMTASKNODES) * ($INSTANCECORES - 2)" | bc -
 BIDPRICE=0.30
 AWS_REGION=us-east-1
 AWS_ZONE=us-east-1e
-NAME='Music Recommender Cluster'
+NAME='Music Recommender Demo Cluster'
 BUCKETNAME=kls-recommendation-spark-emr-demo
 BUCKETS3=s3://$BUCKETNAME
 BUCKETURL="https://s3.amazonaws.com/$BUCKETNAME"
@@ -31,10 +34,15 @@ echo "---------------------------------"
 
 # build and package our Scala code if there was a change
 cd ..
-echo "packaging application code ..."
-mvn -q package
+# For most demos, you will not be interested in rebuilding the Scala code, only
+# in deploying it as-is.  If, on the other hand, you want to tweak the Scala code
+# ane rebuild the Jar files, then uncomment the following lines.  You will
+# need to have Java and Maven installed on the host that you will run this
+# script on.
+echo "building/packaging application jar (if there were changes) ..."  # uncomment to rebuild the Scala Jar
+mvn -q package                      # uncomment to rebuild the Scala Jar
 
-# sync it to s3
+# sync application jar to s3
 echo "syncing application code to s3 ..."
 aws s3 sync target/ $BUCKETS3/ --exclude "*" --include "*.jar" --quiet
 
@@ -51,13 +59,14 @@ aws emr create-cluster --name "$NAME" --ami-version $AMIVERSION \
    Name=Task,InstanceGroupType=TASK,InstanceType=$INST_TYPE,InstanceCount=$NUMTASKNODES,BidPrice=$BIDPRICE \
   --steps Type="Spark",Name="Recommender",ActionOnFailure=CONTINUE,Args=["--deploy-mode","cluster","--class","$CLASS","$BUCKETS3/$JARFILE"] | tee cluster_id
 
-# wait for cluster to finish running the step
+# wait for cluster to finish running the analysis step
 id=`awk '/ClusterId/{ print $2 }' cluster_id | sed 's/"//g'`
-echo "waiting for completion of analysis of cluster $id... check Web Console."
+echo "waiting for completion of analysis by cluster $id..."
+echo "   (You can monitor progress on your AWS Web Console)"
 while true; do
     resp="`aws emr describe-cluster --cluster-id $id`"
     if [ "`echo \"$resp\" | grep 'Waiting after step completed'`" ]; then
-       break
+	break
     fi
     sleep $POLLSECONDS
 done
